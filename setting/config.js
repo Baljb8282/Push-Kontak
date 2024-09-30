660345fc1b7fa9fe91c701b5709cd8012f1a20c0
Baljb8282 <balstore599@gmail.com>
2024-09-30 19:06:30 +0700

    Update config.js
    SV BALL JB

--- setting/config.js
+++ setting/config.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-global.owner = ["6285742632270"];
-global.author = "RezaDevv";
+global.owner = ["6281999330748"];
+global.author = "baljb";
 global.packname = "Push-Kontak";
 global.sessionName = "Bot-Session";
 global.versionbot = "1.2.0";
